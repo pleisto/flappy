686033a0a2e113898d22d7708e9569843c6623e7
darmody <darmody@brickdoc.com>
2023-09-25 12:20:36 +0800

    feat(node-flappy): export the modules and types of node-flappy

--- packages/nodejs/src/index.ts
+++ packages/nodejs/src/index.ts
@@ -3,3 +3,7 @@ export * from './flappy-agent'
 export * from './flappy-type'
 export * from './invoke-function'
 export * from './synthesized-function'
+
+export * from './llm/llm-base'
+export * from './llm/interface'
+export * from './llm/chatgpt'
