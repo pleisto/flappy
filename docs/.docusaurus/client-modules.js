5f243f1666f84e51663f2ef4c6518a53aae199f2
darmody <darmody@brickdoc.com>
2023-10-11 11:31:27 +0800

    style(docs): remove .docusaurus folder

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/ding/Projects/flappy/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/ding/Projects/flappy/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/ding/Projects/flappy/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/ding/Projects/flappy/docs/src/css/theme.css'),
-];
