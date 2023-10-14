629e1b972c587d547ea913d56f7e91e8ae7278a1
Yuhang Shi <clszzyh@gmail.com>
2023-10-14 17:24:52 +0800

    refactor
    Signed-off-by: Yuhang Shi <clszzyh@gmail.com>

--- examples/java/src/main/java/org/example/java/Resume.java
+++ examples/java/src/main/java/org/example/java/Resume.java
@@ -16,39 +16,37 @@ import java.util.concurrent.Future;
 
 public class Resume {
   public static final String RESUME_EXECUTE_PLAN_PROMPT = "Find the resume of a frontend engineer and return their metadata.";
-  static final String MOCK_RESUME_DATA = """
-    I am a seasoned software engineer with over seven years of experience in front-end development. I am passionate about building excellent user interfaces, proficient in HTML, CSS, and JavaScript, and have a deep understanding of front-end frameworks such as React, Vue, and Angular. I have participated in several large-scale projects, responsible for designing and implementing front-end architecture, ensuring high performance and user-friendliness of websites. In addition, I also have project management experience, capable of leading teams to deliver high-quality outputs on time.
-
-    ### Project Experience
-
-    #### 1. E-commerce Website Refactoring (ABC Company)
-    - Participated in the refactoring project of an e-commerce website under ABC Company, serving as the front-end technical lead.
-    - Rebuilt the website frontend using the React framework, implemented responsive design and dynamic loading features, improving the user experience.
-    - Optimized front-end performance, reducing page loading time, and improving the overall website performance.
-    - Designed and implemented a system for user behavior tracking and analysis, providing crucial data support for the marketing team.
-
-    #### 2. Social Media Application Development (XYZ Startup)
-    - Led a four-person front-end development team, developed a social media application from scratch.
-    - Adopted the Vue.js framework and Vuex for state management, implemented real-time chat, post publishing, and user interaction features.
-    - Integrated third-party login and sharing features, enhancing user registration and activity.
-    - Successfully launched the application to the market, with the user count growing from zero to over 50,000.
-
-    #### 3. Internal Management System Upgrade (DEF Enterprise)
-    - Responsible for upgrading the company's internal management system, transitioning from traditional back-end rendering to a modern front-end and back-end separate architecture.
-    - Developed a new front-end interface using the Angular framework, realized fast data loading and interactive features.
-    - Optimized data communication with the back-end using GraphQL, reducing unnecessary request times, and improving system efficiency.
-    - Facilitated the transition of team members to the new technology stack through training and documentation.
-
-    ### Skills and Expertise
-
-    - Front-end technologies: HTML, CSS, JavaScript, React, Vue, Angular, Redux, GraphQL
-    - Front-end tools: Webpack, Babel, ESLint
-    - Project management: Agile, Scrum, Jira
-
-    ### Education
-
-    - Bachelor's Degree, Computer Science, Peking University, 2012
-        """;
+  static final String MOCK_RESUME_DATA = "I am a seasoned software engineer with over seven years of experience in front-end development. I am passionate about building excellent user interfaces, proficient in HTML, CSS, and JavaScript, and have a deep understanding of front-end frameworks such as React, Vue, and Angular. I have participated in several large-scale projects, responsible for designing and implementing front-end architecture, ensuring high performance and user-friendliness of websites. In addition, I also have project management experience, capable of leading teams to deliver high-quality outputs on time.\n" +
+    "\n" +
+    "### Project Experience\n" +
+    "\n" +
+    "#### 1. E-commerce Website Refactoring (ABC Company)\n" +
+    "- Participated in the refactoring project of an e-commerce website under ABC Company, serving as the front-end technical lead.\n" +
+    "- Rebuilt the website frontend using the React framework, implemented responsive design and dynamic loading features, improving the user experience.\n" +
+    "- Optimized front-end performance, reducing page loading time, and improving the overall website performance.\n" +
+    "- Designed and implemented a system for user behavior tracking and analysis, providing crucial data support for the marketing team.\n" +
+    "\n" +
+    "#### 2. Social Media Application Development (XYZ Startup)\n" +
+    "- Led a four-person front-end development team, developed a social media application from scratch.\n" +
+    "- Adopted the Vue.js framework and Vuex for state management, implemented real-time chat, post publishing, and user interaction features.\n" +
+    "- Integrated third-party login and sharing features, enhancing user registration and activity.\n" +
+    "- Successfully launched the application to the market, with the user count growing from zero to over 50,000.\n" +
+    "\n" +
+    "#### 3. Internal Management System Upgrade (DEF Enterprise)\n" +
+    "- Responsible for upgrading the company's internal management system, transitioning from traditional back-end rendering to a modern front-end and back-end separate architecture.\n" +
+    "- Developed a new front-end interface using the Angular framework, realized fast data loading and interactive features.\n" +
+    "- Optimized data communication with the back-end using GraphQL, reducing unnecessary request times, and improving system efficiency.\n" +
+    "- Facilitated the transition of team members to the new technology stack through training and documentation.\n" +
+    "\n" +
+    "### Skills and Expertise\n" +
+    "\n" +
+    "- Front-end technologies: HTML, CSS, JavaScript, React, Vue, Angular, Redux, GraphQL\n" +
+    "- Front-end tools: Webpack, Babel, ESLint\n" +
+    "- Project management: Agile, Scrum, Jira\n" +
+    "\n" +
+    "### Education\n" +
+    "\n" +
+    "- Bachelor's Degree, Computer Science, Peking University, 2012\n";
 
   public static FlappyFunctionBase<?, ?> resumeGetMeta = new FlappySynthesizedFunction(
     "getMeta",
--- packages/kotlin/flappy/src/test/java/flappy/examples/LawTestJava.java
+++ packages/kotlin/flappy/src/test/java/flappy/examples/LawTestJava.java
@@ -19,39 +19,35 @@ public class LawTestJava {
       switch (source) {
         case FlappyBaseAgent.AGENT_SOURCE:
           return new LLMResponse.Success(
-            """
-              [
-                {
-                  "id": 1,
-                  "functionName": "getLatestLawsuitsByPlaintiff",
-                  "args": {
-                    "plaintiff": "families of victims",
-                    "arg1": false
-                  },
-                  "thought": "Get the latest lawsuits where the plaintiff is families of victims."
-                },
-                {
-                  "id": 2,
-                  "functionName": "getMeta",
-                  "args": {
-                    "lawsuit": "%@_1"
-                  },
-                  "thought": "Extract metadata from the lawsuit."
-                }
-              ]
-                """);
+            "[\n" +
+            "  {\n" +
+            "    \"id\": 1,\n" +
+            "    \"functionName\": \"getLatestLawsuitsByPlaintiff\",\n" +
+            "    \"args\": {\n" +
+            "      \"plaintiff\": \"families of victims\",\n" +
+            "      \"arg1\": false\n" +
+            "    },\n" +
+            "    \"thought\": \"Get the latest lawsuits where the plaintiff is families of victims.\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"id\": 2,\n" +
+            "    \"functionName\": \"getMeta\",\n" +
+            "    \"args\": {\n" +
+            "      \"lawsuit\": \"%@_1\"\n" +
+            "    },\n" +
+            "    \"thought\": \"Extract metadata from the lawsuit.\"\n" +
+            "  }\n" +
+            "]\n");
       }
 
       if (source.equals(lawGetMeta.getSource())) {
         return new LLMResponse.Success(
-          """
-              {
-              "verdict": "Unknown",
-              "plaintiff": "families of victims of Sandy Hook Elementary School shooting",
-              "defendant": "Alex Jones",
-              "judgeOptions": ["reduce personal expenses to a reasonable level", "bar from further waste of estate assets", "appoint a trustee to oversee spending", "dismiss the bankruptcy case"]
-            }
-            """.trim());
+          ("  {\n" +
+           "  \"verdict\": \"Unknown\",\n" +
+           "  \"plaintiff\": \"families of victims of Sandy Hook Elementary School shooting\",\n" +
+           "  \"defendant\": \"Alex Jones\",\n" +
+           "  \"judgeOptions\": [\"reduce personal expenses to a reasonable level\", \"bar from further waste of estate assets\", \"appoint a trustee to oversee spending\", \"dismiss the bankruptcy case\"]\n" +
+           "}\n").trim());
       }
       return new LLMResponse.Success("");
     }));
