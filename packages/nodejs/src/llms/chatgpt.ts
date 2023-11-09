3bfed48e03e54ba48fb04684b7e746fa537896cd
Yuhang Shi <clszzyh@gmail.com>
2023-11-09 14:28:32 +0800

    feat(nodejs): openai gpt3 json response format
    Signed-off-by: Yuhang Shi <clszzyh@gmail.com>

--- packages/nodejs/src/llms/chatgpt.ts
+++ packages/nodejs/src/llms/chatgpt.ts
@@ -2,6 +2,10 @@ import { LLMBase } from './llm-base'
 import { type ChatMLMessage, type ChatMLResponse, type GenerateConfig } from './interface'
 import type OpenAI from 'openai'
 import { log } from '../utils'
+import {
+  type ChatCompletionCreateParamsNonStreaming,
+  type ChatCompletionCreateParamsBase
+} from 'openai/resources/chat/completions'
 
 /**
  * Calculate the default max tokens for a given model.
@@ -9,31 +13,48 @@ import { log } from '../utils'
  * @param model Model name.
  * @returns
  */
-const calcDefaultMaxTokens = (model: string): number => {
+const calcDefaultMaxTokens = (model: ChatGPTModel): number => {
   if (model.includes('16k')) return 16385
   if (model.includes('32k')) return 32768
   if (model.includes('gpt-4')) return 8192
   return 4096
 }
 
+// https://platform.openai.com/docs/api-reference/chat/create#chat-create-response_format
+const allowJsonResponseFormat = (model: ChatGPTModel): boolean => {
+  return model === jsonObjectModel
+}
+
+type ChatGPTModel = ChatCompletionCreateParamsBase['model']
+
+export interface ChatGPTOptions {
+  model?: ChatGPTModel
+  maxTokens?: number
+}
+
+const jsonObjectModel: ChatGPTModel = 'gpt-3.5-turbo-1106'
+
 export class ChatGPT extends LLMBase {
   client: OpenAI
   override maxTokens: number
-  model: string
-  constructor(client: OpenAI, model: string, maxTokens?: number) {
+  model: ChatGPTModel
+  constructor(client: OpenAI, options?: ChatGPTOptions) {
+    const model = options?.model ?? jsonObjectModel
     super()
     this.client = client
     this.model = model
-    this.maxTokens = maxTokens ?? calcDefaultMaxTokens(model)
+    this.maxTokens = options?.maxTokens ?? calcDefaultMaxTokens(model)
   }
 
   async chatComplete(messages: ChatMLMessage[], config?: GenerateConfig): Promise<ChatMLResponse> {
-    const resp = await this.client.chat.completions.create({
+    const args: ChatCompletionCreateParamsNonStreaming = {
       model: this.model,
       messages,
       max_tokens: config?.maxTokens,
+      ...(allowJsonResponseFormat(this.model) ? { response_format: { type: 'json_object' } } : {}),
       ...config
-    })
+    }
+    const resp = await this.client.chat.completions.create(args)
     const choice = resp.choices[0]
     if (!choice) return { success: false, data: undefined }
 
