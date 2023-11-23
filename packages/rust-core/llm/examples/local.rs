e208885829b0d758e95b239a5d458731c0419034
Yuhang Shi <clszzyh@gmail.com>
2023-11-23 14:23:31 +0800

    feat: sampler
    Signed-off-by: Yuhang Shi <clszzyh@gmail.com>

--- packages/rust-core/llm/examples/local.rs
+++ packages/rust-core/llm/examples/local.rs
@@ -2,30 +2,35 @@ use flappy_llm::{
   client::Client,
   local::client::{LocalClient, LocalOptionsBuilder},
   model::{ChatMLMessage, Prompt},
+  options::BuiltinOptionsBuilder,
 };
 use hf_hub::api::tokio::Api;
 use tracing::info;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-  // std::env::set_var("RUST_LOG", "DEBUG");
+  std::env::set_var("RUST_LOG", "DEBUG");
   tracing_subscriber::fmt::init();
 
   let api = Api::new().unwrap();
   let repo = api.model("rustformers/open-llama-ggml".to_string());
   let filename = repo.get("open_llama_3b-f16.bin").await.unwrap();
 
-  let options = LocalOptionsBuilder::default()
-    .model_arch(Some("llama".to_string()))
+  let local_options = LocalOptionsBuilder::default()
     .model_path(Some(filename))
     .build()
     .unwrap();
-  let client = LocalClient::new_with_custom_options(options).unwrap();
+  let client = LocalClient::new_with_custom_options(local_options).unwrap();
 
   let result = client
     .chat_complete(
       Prompt::new_chat(vec![ChatMLMessage::user("who are you".to_string())]),
-      Default::default(),
+      BuiltinOptionsBuilder::default()
+        .top_k(Some(35.0))
+        .top_p(Some(0.9))
+        .temperature(Some(0.7))
+        .build()
+        .unwrap(),
     )
     .await
     .unwrap();
--- packages/rust-core/llm/src/local/client.rs
+++ packages/rust-core/llm/src/local/client.rs
@@ -1,4 +1,4 @@
-use std::{convert::Infallible, path::PathBuf};
+use std::{convert::Infallible, io::Write, path::PathBuf};
 
 use anyhow::anyhow;
 use async_trait::async_trait;
@@ -16,16 +16,61 @@ use crate::{
   options::{BuiltinOptions, Options},
 };
 
-#[derive(Default, Builder)]
+#[derive(Builder)]
+#[builder(default)]
 pub struct LocalOptions {
   model_arch: Option<String>,
   model_path: Option<PathBuf>,
+  /// The context size ("memory") the model should use when evaluating a prompt. A larger context
+  /// consumes more resources, but produces more consistent and coherent responses.
+  /// The number of bits used for context_size (2^n), default is 13 (8192)
+  context_size: u32,
+}
+
+impl Default for LocalOptions {
+  fn default() -> Self {
+    Self {
+      context_size: 13,
+      model_arch: Some("llama".to_string()),
+      model_path: Default::default(),
+    }
+  }
 }
 
 pub struct LocalClient {
   model_client: Box<dyn Model>,
   #[allow(dead_code)]
   options: Options<LocalOptions>,
+}
+
+impl From<&Options<LocalOptions>> for ModelParameters {
+  fn from(value: &Options<LocalOptions>) -> Self {
+    Self {
+      context_size: 2_usize.pow(value.custom.context_size),
+      ..Default::default()
+    }
+  }
+}
+
+// https://github.com/rustformers/llm/blob/main/crates/llm-base/src/samplers.rs#L241
+impl BuiltinOptions {
+  fn sampler_options(&self) -> Vec<String> {
+    let mut options = vec![];
+
+    if let Some(temperature) = self.temperature {
+      options.push(format!("temperature:{}", temperature))
+    }
+
+    if let Some(top_p) = self.top_p {
+      options.push(format!("topp:p={}", top_p))
+    }
+
+    if let Some(top_k) = self.top_k {
+      options.push(format!("topk:k={}", top_k))
+    }
+
+    options
+  }
 }
 
 #[async_trait]
@@ -53,7 +98,7 @@ impl Client for LocalClient {
           "Model path is empty"
         )))?,
       TokenizerSource::Embedded,
-      ModelParameters::default(),
+      (&options).into(),
       load_progress_callback_stdout,
     )
     .map_err(|e| ClientCreationError::InnerError(Box::new(e)))?;
@@ -69,16 +114,19 @@ impl Client for LocalClient {
   async fn chat_complete(
     &self,
     prompt: crate::model::Prompt,
-    _: BuiltinOptions,
+    opt: BuiltinOptions,
   ) -> Result<Output, ExecutorError> {
     // https://github.com/rustformers/llm/blob/main/binaries/llm-test/src/inference.rs#L23
     let session = &mut self.model_client.start_session(Default::default());
     let mut output = String::new();
     let prompt = prompt.to_string();
 
+    let sampler = llm::samplers::build_sampler(20000, &[], &opt.sampler_options())
+      .map_err(|err| ExecutorError::InnerError(Box::new(err)))?;
+
     let request = InferenceRequest {
       prompt: llm::Prompt::Text(prompt.as_str()),
-      parameters: &InferenceParameters::default(),
+      parameters: &InferenceParameters { sampler },
       play_back_previous_tokens: false,
       maximum_token_count: None,
     };
@@ -94,13 +142,17 @@ impl Client for LocalClient {
         &mut Default::default(),
         |t| match t {
           llm::InferenceResponse::PromptToken(t) | llm::InferenceResponse::InferredToken(t) => {
+            print!("\x1b[31m{t}\x1b[0m");
+            let Ok(_) = std::io::stdout().flush() else {
+              return Ok(llm::InferenceFeedback::Halt);
+            };
             output += &t;
             Ok(llm::InferenceFeedback::Continue)
           }
           _ => Ok(llm::InferenceFeedback::Continue),
         },
       )
-      .map_err(|e| ExecutorError::InnerError(Box::new(e)))?;
+      .map_err(|err| ExecutorError::InnerError(Box::new(err)))?;
 
     info!("output {}", output);
 
